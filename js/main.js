6ee977eb95a53d98432a31533767066206e04555
mcle0341 <mcle0341@algonquinlive.com>
2016-04-15 15:20:16 -0400

    removed space

--- js/main.js
+++ js/main.js
@@ -1,41 +1,35 @@
 var $cat = $('.cat');
+var $medical = $('.medical');
+var $grooming = $('.grooming');
+var $food = $('.food');
+var $litter = $('.litter');
+var $toys = $('.toys');
+var $total = $('.total');
 
 $cat.waypoint(function () {
   $cat.addClass('js-svg-animate');
-}, { offset: '50%'});
-
-var $medical = $('.medical');
+}, {offset: '50%'});
 
 $medical.waypoint(function () {
   $medical.addClass('js-svg-animate');
-}, { offset: '50%'});
-
-var $grooming = $('.grooming');
+}, {offset: '50%'});
 
 $grooming.waypoint(function () {
   $grooming.addClass('js-svg-animate');
-}, { offset: '50%'});
-
-var $food = $('.food');
+}, {offset: '50%'});
 
 $food.waypoint(function () {
   $food.addClass('js-svg-animate');
-}, { offset: '50%'});
-
-var $litter = $('.litter');
+}, {offset: '50%'});
 
 $litter.waypoint(function () {
   $litter.addClass('js-svg-animate');
-}, { offset: '50%'});
-
-var $toys = $('.toys');
+}, {offset: '50%'});
 
 $toys.waypoint(function () {
   $toys.addClass('js-svg-animate');
-}, { offset: '50%'});
-
-var $total = $('.total');
+}, {offset: '50%'});
 
 $total.waypoint(function () {
   $total.addClass('js-svg-animate');
-}, { offset: '50%'});
+}, {offset: '50%'});
